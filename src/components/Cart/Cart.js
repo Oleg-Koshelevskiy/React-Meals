58fc23c62c7eabc4a5215cfadcc16bb7598b11e2
Oleg <oleg17.k@gmail.com>
2022-08-12 10:54:54 +0300

    fixed header for mobile

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -75,7 +75,7 @@ const Cart = (props) => {
 
   const cartModalContent = (
     <React.Fragment>
-      {cartItems}
+      {!isCheckout && cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
